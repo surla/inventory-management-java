9f9ebbcbcd8744e4af137290f415c4e024c7c47c
Joseph Surla <josephsurla@gmail.com>
2020-09-06 14:23:07 +0900

    Add cancelButtonOnClick method

--- src/View_Controller/AddPartController.java
+++ src/View_Controller/AddPartController.java
@@ -1,4 +1,22 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddPartController {
+    @FXML
+    private void cancelButtonOnClick(ActionEvent event) throws IOException {
+        Parent addPartParent = FXMLLoader.load(getClass().getResource("/View_Controller/InventoryMain.fxml"));
+        Scene addPartScene = new Scene(addPartParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(addPartScene);
+        window.show();
+    }
 }
