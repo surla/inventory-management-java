d608b44fdfa1118c449bde78fd3d7dfc84578395
Joseph Surla <josephsurla@gmail.com>
2020-09-06 14:04:21 +0900

    Add addPartSceneOnClick method to InventoryMain

--- src/View_Controller/InventoryMainController.java
+++ src/View_Controller/InventoryMainController.java
@@ -1,4 +1,62 @@
 package View_Controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class InventoryMainController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableColumn<?, ?> partTable;
+
+    @FXML
+    private Button addPartButton;
+
+    @FXML
+    private Button modifyPartButton;
+
+    @FXML
+    private Button deletePartButton;
+
+    @FXML
+    private Button searchPartButton;
+
+    @FXML
+    private TextField searchPartField;
+
+    /**
+     * This method when called will change the Scene to Add Part form
+     *
+     */
+
+    @FXML
+    private void addPartSceneOnClick(ActionEvent event) throws IOException {
+        Parent addPartParent = FXMLLoader.load(getClass().getResource("/View_Controller/AddPart.fxml"));
+        Scene addPartScene = new Scene(addPartParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(addPartScene);
+        window.show();
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
