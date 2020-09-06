873b6d28be6371feb7b18ab968003ae108428184
Joseph Surla <josephsurla@gmail.com>
2020-09-06 16:25:50 +0900

    Create Inventory and Products class

---
+++ src/Model/Inventory.java
@@ -0,0 +1,12 @@
+package Model;
+
+import javafx.collections.ObservableList;
+
+public class Inventory {
+    private ObservableList<Part> allParts;
+    private ObservableList<Product> allProducts;
+
+    public Inventory() {
+
+    }
+}
